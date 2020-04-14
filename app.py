7b404b0ba972c00b48a6b6c432da0b77e8a6e140
Michael Stallter <mstallter@comcast.net>
2020-04-14 16:43:10 -0500

    changed db stuff

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from functools import wraps
-
+import os
 import pymysql
-import secrets
+#import secrets
 
 
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+#conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
 # Open database connection
-#dbhost = secrets.dbhost
-#dbuser = secrets.dbuser
-#dbpass = secrets.dbpass
-#dbname = secrets.dbname
+dbhost = os.environ.get('DBHOST')
+dbuser = os.environ.get('DBUSER')
+dbpass = os.environ.get('DBPASS')
+dbname = os.environ.get('DBNAME')
 
-#db = pymysql.connect(dbhost, dbuser, dbpass, dbname)
+db = pymysql.connect(dbhost, dbuser, dbpass, dbname)
 
 app = Flask(__name__)
 
